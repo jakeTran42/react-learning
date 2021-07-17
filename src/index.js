65acd96b2ff0ea6cd1c7fb0796d0e6ff7f6c2554
Jake Tran <jake.tranehs@gmail.com>
2021-07-17 04:13:17 -0700

    update handleCLick to relfect current move

--- src/index.js
+++ src/index.js
@@ -53,11 +53,12 @@ class Game extends React.Component {
         },
       ],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -72,18 +73,26 @@ class Game extends React.Component {
         },
       ]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    const moves = history.map((steps, move) => {
+    const moves = history.map((step, move) => {
       const desc = move ? "Jump to #" + move : "Go to game start";
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
